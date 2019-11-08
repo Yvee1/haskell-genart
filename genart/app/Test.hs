a005001ac440a8a1e1acc949a04030bdc053e3e2
Yvee1 <stevenvdb@live.nl>
2019-11-08 23:21:02 +0100

    add really basic circle type

--- genart/app/Test.hs
+++ genart/app/Test.hs
@@ -12,14 +12,23 @@ darkGunmetal = hsva 170 0.30 0.16
 
 renderSketch :: Generate ()
 renderSketch = do
-  fillScreen eggshell 1
+  fillScreen darkGunmetal 1
   (w, h) <- getSize @Double
 
   cairo $ do 
     setLineWidth 0.35
+
+    draw $ circle (w/2 .& h/2) 25
+    eggshell 1
+    fill
+
     draw $ pentagon (w/2 .& h/2) 25
     darkGunmetal 1
     fill
+
+    -- draw $ triangle (w/2 .& h/2) 20
+    -- eggshell 1
+    -- fill
 
 test :: IO ()
 test = outputSketch (100, 100, 10) renderSketch
--- genart/src/Genart/Shapes/Circle.hs
+++ genart/src/Genart/Shapes/Circle.hs
@@ -3,3 +3,12 @@ module Genart.Shapes.Circle where
 
 import Genart.CairoHelpers
 import Genart.Shapes.Types
+
+data Circle = Circle Pt Double
+  deriving (Show, Eq)
+
+instance Draw Circle where
+  draw (Circle (P (V2 x y)) r) = arc x y r 0 (2 * pi)
+
+circle :: Pt -> Double -> Circle
+circle = Circle
