a5acf57fd2bc357f11c215967506401dd3ed6137
Steven van den Broek <stevenvdb@live.nl>
2020-04-24 23:43:28 +0200

    Tested cairo output to SVG. It works! (for Render)

--- genart/app/Main.hs
+++ genart/app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
 import Sketches
+import GI.Cairo.Render
+
+sketch = svgTest
 
 main :: IO ()
-main = svgTest
+main = Main.test
+
+mySketch :: Render ()
+mySketch = do
+  setSourceRGBA 1 1 0 1
+  rectangle 10 10 100 100
+  fill
+
+test :: IO ()
+test = 
+  withSVGSurface "out.svg" 500 500 $ \surface -> do
+    renderWith surface mySketch
+    surfaceFinish surface
